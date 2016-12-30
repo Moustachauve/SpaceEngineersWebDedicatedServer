456e5dfe489812e162d8fd71b4fd1ade960b8ff0
fvgoulet <flx890@gmail.com>
2016-12-30 01:20:45 -0500

    Handled the case where SpaceEngineers has been discovered on drive and not properly installed

--- ServerExtender/Scripts/GatherGameFiles.ps1
+++ ServerExtender/Scripts/GatherGameFiles.ps1
@@ -4,11 +4,16 @@
   [string]$ProjectFolder
 )
 $ErrorActionPreference = 'Stop'
-$spaceEngineersInstallationKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 244850'
+$spaceEngineersInstallationKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 244851'
 $contentFolders = @('Data', 'Models', 'Particles', 'VoxelMaps')
-try
-{
-  $GamePath = (Get-ItemProperty -Path $spaceEngineersInstallationKey).InstallLocation
+try {
+  $GamePath = (Get-ItemProperty -Path $spaceEngineersInstallationKey -ErrorAction SilentlyContinue).InstallLocation
+  if(-Not $GamePath) {
+    $GamePath = "$((Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Valve\Steam').InstallPath)\steamapps\common\SpaceEngineers"
+    if(-Not $(Test-Path -Path $GamePath)) {
+      throw "The heck, where is your Space Engineers Installation!?"
+    }
+  }
   $contentFolders | %{ Copy-Item -Path "$GamePath\Content\$_" -Destination "$PsScriptRoot\..\bin\x64\Content\" -Recurse -Force -Verbose }
   Copy-Item -Path "$GamePath\DedicatedServer64\*" -Destination "$PSScriptRoot\..\GameFiles" -Recurse -Force -Verbose
 } catch {
